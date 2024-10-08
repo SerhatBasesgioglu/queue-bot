d316dfe52d7439b4a38c013fa439bb423a61ded5
Serhat Başesgioğlu <55340640+SerhatBasesgioglu@users.noreply.github.com>
2024-10-08 11:21:26 +0300

    add lobbystate enum

--- src/classes/lobby.js
+++ src/classes/lobby.js
@@ -1,10 +1,11 @@
+import LobbyState from "../enums/lobbyState.js";
 import LobbyStatus from "../enums/lobbyStatus.js";
 
 export class Lobby {
     constructor(id, maxPlayer = 6) {
         this.id = id;
         this.members = new Set();
-        this.status = "open";
+        this.state = LobbyState.QUEUE;
         this.createdTime = new Date();
         this.maxPlayer = maxPlayer;
         console.log(`Lobby with id ${this.id} has been created!`);
@@ -29,5 +30,21 @@ export class Lobby {
         if (!this.members.has(memberId)) return LobbyStatus.NOT_IN_LOBBY;
         this.members.delete(memberId);
         return LobbyStatus.SUCCESS;
+    }
+
+    startCaptainSelection() {
+        this.state = LobbyState.CAPTAIN_SELECTION;
+    }
+
+    startMapSelection() {
+        this.state = LobbyState.MAP_SELECTION;
+    }
+
+    startGame() {
+        this.state = LobbyState.GAME;
+    }
+
+    endGame() {
+        this.state = LobbyState.END_SCREEN;
     }
 }
---
+++ src/enums/lobbyState.js
@@ -0,0 +1,8 @@
+const LobbyState = {
+    QUEUE: 1,
+    CAPTAIN_SELECTION: 2,
+    MAP_SELECTION: 3,
+    GAME: 4,
+    END_SCREEN: 5,
+};
+export default LobbyState;
